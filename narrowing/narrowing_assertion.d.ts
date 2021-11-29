9a4f7a410de3f5c2de4e0a1089d6f3fdf3bd05ee
cre4tive97 <harryborrison5148@gmail.com>
2021-11-30 06:44:09 +0900

    narrowing & assertion

---
+++ narrowing/narrowing_assertion.d.ts
@@ -0,0 +1,16 @@
+declare function 내함수(x: number | string): string | number;
+declare function 내함수2(x: number | string): void;
+declare function 내함수3(x: number | string): void;
+declare function clean(arr: (string | number)[]): number[];
+declare let 철수쌤: {
+    subject: string;
+};
+declare let 영희쌤: {
+    subject: string[];
+};
+declare let 민수쌤: {
+    subject: string[];
+};
+declare function subject(teacher: {
+    subject: string | string[];
+}): string;
---
+++ narrowing/narrowing_assertion.ts
@@ -0,0 +1,43 @@
+function 내함수(x :number | string) {
+    if (typeof x === 'string') return x + '1';
+    else return x + 1;
+}
+내함수(123);
+
+function 내함수2(x :number|string) {
+    let array :number[] = [];
+    if (typeof x === 'number') array[0] = x;
+}
+
+function 내함수3(x :number|string) {
+    let array :number[] = [];
+    array[0] = x as number;
+}
+
+// Type Assertion (as) 는 언제 쓰는가?
+// type narrowing 할 때 씀
+// 어떤 타입이 들어올 지 100% 확실할 때 쓰셈
+// 근데 그렇게 되면 TS쓰는 의미 없어지니 디버깅용으로만 쓰셈
+
+
+function clean(arr :(string|number)[]) :number[] {
+    return arr.map(item => {
+        if(typeof item === 'string') return parseFloat(item);
+        else return item;
+    });
+}
+console.log(clean([1,2,'3','4',5]))
+
+let 철수쌤 = { subject : 'math' }
+let 영희쌤 = { subject : ['science', 'english'] }
+let 민수쌤 = { subject : ['science', 'art', 'korean'] }
+
+function subject(teacher :{subject : string|string[]}) :string {
+    if(typeof teacher.subject === 'string') return teacher.subject;
+    else if(Array.isArray(teacher.subject)) return teacher.subject[teacher.subject.length-1];
+    else return '없쪄';
+}
+
+console.log(subject(철수쌤))
+console.log(subject(영희쌤))
+console.log(subject(민수쌤))
